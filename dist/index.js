ea7391e756a90f79b7ece1f2ded93723191633b7
Popescu <florin.popescu0110@gmail.com>
2016-08-02 15:57:55 +0300

    Added commonjs compiler to build files

--- dist/index.js
+++ dist/index.js
@@ -1,44 +1,51 @@
 "use strict";
 
-System.register(["font-awesome/./css/font-awesome.min.css!", "jquery", "froala-editor/./js/froala_editor.min", "froala-editor/css/froala_editor.min.css!", "froala-editor/css/froala_style.min.css!"], function (_export, _context) {
-	"use strict";
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.configure = configure;
 
-	var $, froala;
-	return {
-		setters: [function (_fontAwesomeCssFontAwesomeMinCss) {}, function (_jquery) {
-			$ = _jquery.default;
-		}, function (_froalaEditorJsFroala_editorMin) {
-			froala = _froalaEditorJsFroala_editorMin;
-		}, function (_froalaEditorCssFroala_editorMinCss) {}, function (_froalaEditorCssFroala_styleMinCss) {}],
-		execute: function () {
-			function configure(aurelia, config) {
-				aurelia.globalResources('./aurelia-froala');
-				var c = {
-					setLicense: function setLicense(license) {
-						$.FroalaEditor.DEFAULTS.key = license;
-					},
-					addPlugin: function addPlugin(name) {
-						return Promise.all([System.import("froala-editor/js/plugins/" + name + ".min", _context.id).then(function (m) {
-							return m();
-						}), System.import("froala-editor/css/plugins/" + name + ".css!", _context.id).catch(function (e) {})]);
-					},
-					global: function global(callback) {
-						callback($.FroalaEditor);
-					},
-					addDefaltOptions: function addDefaltOptions(options) {
-						$.FroalaEditor.DEFAULTS = $.extend($.FroalaEditor.DEFAULTS, options);
-					},
-					addLanguage: function addLanguage(language, additionalTranslations) {
-						return System.import("froala-editor/js/languages/" + language, _context.id).then(function () {
-							return additionalTranslations && Object.assign($.FE.LANGUAGE[language].translation, additionalTranslations);
-						});
-					}
-				};
-				froala.default();
-				config(c);
-			}
+require("font-awesome/./css/font-awesome.min.css!");
 
-			_export("configure", configure);
+var _jquery = require("jquery");
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+var _froala_editor = require("froala-editor/./js/froala_editor.min");
+
+var froala = _interopRequireWildcard(_froala_editor);
+
+require("froala-editor/css/froala_editor.min.css!");
+
+require("froala-editor/css/froala_style.min.css!");
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function configure(aurelia, config) {
+	aurelia.globalResources('./aurelia-froala');
+	var c = {
+		setLicense: function setLicense(license) {
+			_jquery2.default.FroalaEditor.DEFAULTS.key = license;
+		},
+		addPlugin: function addPlugin(name) {
+			return Promise.all([System.import("froala-editor/js/plugins/" + name + ".min", __moduleName).then(function (m) {
+				return m();
+			}), System.import("froala-editor/css/plugins/" + name + ".css!", __moduleName).catch(function (e) {})]);
+		},
+		global: function global(callback) {
+			callback(_jquery2.default.FroalaEditor);
+		},
+		addDefaltOptions: function addDefaltOptions(options) {
+			_jquery2.default.FroalaEditor.DEFAULTS = _jquery2.default.extend(_jquery2.default.FroalaEditor.DEFAULTS, options);
+		},
+		addLanguage: function addLanguage(language, additionalTranslations) {
+			return System.import("froala-editor/js/languages/" + language, __moduleName).then(function () {
+				return additionalTranslations && Object.assign(_jquery2.default.FE.LANGUAGE[language].translation, additionalTranslations);
+			});
 		}
 	};
-});
+	froala.default();
+	config(c);
+}
